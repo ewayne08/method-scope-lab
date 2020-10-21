0100b892fdc04e3456208f882e228e2f9fee7faf
Ethan <ethanchristianwayne@gmail.com>
2020-10-21 17:58:01 -0400

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,16 @@
+status = "Not rescued"
+
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
+def find_toadstool
+  puts status
+end
+
+
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+rescue_princess_peach
+puts status
